d3c202f4bd12484bd1f2c140baddf65e75ed341f
jason c. kay <j@son-kay.com>
2019-02-14 15:15:15 -0500

    Update to reflect changes for JSON tool

---
+++ champions-rose.php
@@ -0,0 +1,182 @@
+<?php require_once('services/solarian.php'); ?><!DOCTYPE html>
+	<html lang="en-uk" class="no-js" prefix="og: http://ogp.me/ns#">
+	<?php pageHeader("meta.json"); ?>
+		<body>
+			 <?php menubar(); ?>
+			<section class="rose-area relative" id="home">
+				<div class="overlay overlay-bg"></div>	
+				<div class="container">
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="banner-content col-lg-8">
+							<br /><br />
+							<h1 class="text-white">Champion's Rose</h1>
+								<span class="bng-header bng-clanbanner"></span>
+								<p class="text-white">Our Season of the Drifter Community PvP Tournament</p>
+							</p>
+						</div>											
+					</div>
+				</div>					
+			</section>		
+			<!-- Start Align Area -->
+			<section class="about-generic-area section-gap">
+				<div class="container border-top-generic">
+					<h2 class="about-title mb-30">Tournament Rules</h2>
+					<h3 class="about-title mb-30">Overview</h3>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="img-text">
+								<img src="img/champions-rose.png" alt="" class="img-fluid float-left mr-20 mb-20">
+								<p align="justify">This tournament will be conducted over the span of the Season of the Drifter.  Each qualifying competitor will play Rumble matches each week they are available, earning points based on their overall finish placement for the week.  Competitors are not tied into any subclass or loadout for the duration of the tournament, but are restricted based on the rules for matches within a given week.</p>
+
+								<p align="justify">Points will be awarded using a tiered merit system centered around finish placement across all matches each week, primarily driven by kills.  Additionally, after the first two weeks, a catch-up mechanic will take effect which will allow players behind in the rankings due to missing a week or prior poor performance to more quickly gain ground by defeating high-ranked players, while simultaneously rewarding consistent exceptional performance.  In the final three weeks, the catch-up mechanic will intensify in a sprint to the finish.</p>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<p align="justify">This tournament will be administered through the Solarian community Discord server, with players registering for the tournament, selecting their weekly class, subclass, and loadout, and posting match results in a designated channel.  On the backend, the tournament staff will track players' progress with a custom-built utility and post weekly standings, results, rulings, and other announcements in the same channels.</p>
+						</div>
+						<h3 class="about-title mb-30">Eligibility</h3>
+						<div class="col-lg-12">
+							<p align="justify">This tournament will open to all members, beginners, and friends of Solarian Clan in good standing, as well as any active members of the Solarian Clan Discord on the PlayStation 4™, Xbox™, or BattleNet™ platforms.  To be eligible, all participants must be in good standing in the Discord, be registered with our clan stats bot, and have the ability to capture and send screenshots directly from the game to the Discord (not by taking a photograph of the screen).</p>
+							
+							<p align="justify">All eligible players wishing to participate will need to complete and submit a registration form, including a working e-mail address.  All tournament reminders will be sent out via Discord, Twitter, and E-Mail.  Additionally, all results will be posted on the clan website and announced in a short video.</p>
+							
+							<p align="justify">Players are not required to play every week, but will not receive points for weeks in which they do not play.  There will be no disqualifications due to not playing.  Players disqualified for rules violations during this tournament or in previous tournaments are ineligible to participate.</p>
+						</div>
+						<h3 class="about-title mb-30">Registration</h3>
+						<div class="col-lg-12">
+							<p align="justify">Registration for this tournament will be open from Tuesday, 12 February 2019 through the start of the first match on Friday, 22 Feburary 2019.</p>
+							
+							<p align="justify">To register, players must read these rules and complete the registration form found at <a href="https://solarian.net/registration">https://solarian.net/registration</a>.</p>
+							
+							<p align="justify">Once registered, players will be given read access to the #champions-rose Discord channel, for the review of tournament updates, match results, weekly leaderboards, and annoucements related to the competition.</p>
+						</div>
+						<h3 class="about-title mb-30">Tournament Structure</h3>
+						<div class="col-lg-12">
+							<p align="justify">Players in the tournament may participate in one or more tournament PvP Rumble private matches each week.</p>
+								
+							<p align="justify">Scheduled matches will be held each Friday, Saturday, and Sunday at varying times to accomodate players in various timezones.  <b>ALL MATCH TIMES WILL BE POSTED IN THE GMT/UTC TIMEZONE.</b>  These matches will be posted in the #champions-rose-matches channel, managed by @Apollo's event tools.  To RSVP and claim a slot in a particular match, simply select the "+" icon beneath the match time of your choosing.  Up to eight (8) players can participate in a single match.  If more than eight players wish to participate at the same time, multiple matches should be held, with players breaking into groups of at least four.</p>
+								
+							<p align="justify">Additionally, ad-hoc matches may be scheduled by any participant.  To schedule one of these matches, go to the #tournament-discussions channel and issue the command "=event", and follow the prompts messaged to you by Apollo.  Please set the match description to "[Platform] Ad hoc // Loadout Rules // Map" (replacing <b>[Platform]</b> with [PS], [XB], or [PC], <b>Loadout Rules</b> with the title of the Loadout model found on the tournament schedule, and <b>Map</b> with the name of the week's map).  The time and date for your match must fall before the next reset (when the current bracket ends).  Match times must be provided in UTC/GMT.</p>
+
+							<p align="justify"><b>ALL MATCHES MUST HAVE AT LEAST FOUR COMPETITORS TO BE VALID.</b></p>
+							
+							<p align="justify">Upon completion of the match, all players should capture the Post-game Carnage Report (PGCR) screen.  The fireteam leader for the match should attach their screenshot in either a Discord direct message or a platform messaging service direct message to soren42 (soren42 on Discord, PlayStation, and XBox; soren42#1496 on BattleNet).  Please do not take photographs of the screen and submit them, as the results are being processed by a program.</p>
+								
+							<p align="justify">Players will be awarded points based on their finish rank, as determined across all matches held, first on the number of kills they obtained, and second on the amount of damage done.  Scoring details may be found in the section below entitled, "Scoring".  If a player participates in multiple matches in a given week, only their best performance will be counted.</p>
+							
+							<p align="justify">At various points during the tournament, additional "Foothold" matches may be scheduled by the admin team.  These matches are in addition to the regular tournament matches; they will utilise different rules and a unique scoring model that will allow players the opportunity to obtain a small amount of additional bonus points during these weeks.  Players may not schedule ad hoc Foothold matches.</p>
+							
+							<p align="justify">At the conclusion of each week, a leaderboard will be posted with all competitors cumulative point tallies and current rankings.  Those standings will remain in effect until the following week's leaderboard is posted (for scoring purposes, as indicated below).</p>
+							
+							<p align="justify">If a competitor is unable to participate in a given week, they simply will not receive any points that week.</p>
+
+							<p align="justify">Players may change their class and subclass from match to match.  Each week will feature different ability restrictions and loadout rules, ranging from "Anything Goes" to curated loadouts.  More on each loadout's rules can be found below in the section "Loadouts".  Players may not change their class, subclass, weapon loadouts, perks, or any game options once a match begins (as determined by the fireteam leader selecting "Launch" from the orbit screen).  All players are required to go to the #champions-rose-loadouts channel and issue the command "?loadout" and display the loadout they will be using prior to match start.</p>
+						</div>
+						<h3 class="about-title mb-30">Match Settings</h3>
+						<div class="col-lg-12">
+							<p align="justify">The following private match settings should be employed by the fireteam leader (selected by the match participants):<br />
+							<ul class="unordered-list">		
+								<li><b>Game Type:</b> Rumble</li>
+								<li><b>Match Time Limit:</b> 30 minutes</li>
+								<li><b>Match Score Limit:</b> 125</li>
+								<li><b>Respawn Timer:</b> 2 seconds</li>
+							</ul>
+							The map changes from week to week.  The weekly map is listed on the schedule, along with loadout details for the week.</p>
+						
+							<p align="justify">The match ends when either 30 minutes has elapsed or a player has reached 125 kills.</p>
+						</div>
+						<h3 class="about-title mb-30">Scoring</h3>
+						<div class="col-lg-12">
+							<p align="justify">Tournament scoring utilises a points-based system with four models of progressive rewards based on finish rank.</p>  
+							
+							<p align="justify"><b>PLEASE NOTE:</b> Several models utilise the notation "+&Delta;##%" &mdash; this indicates that in addition to the flat amount of points awarded, a player finishing in this position will also earn the indicated percentage difference between the number of tournament points they began the week with and the number of points that the overall leader from the previous week began the week with.  For example, if a player begins the week with 50 points and the current player at the top of the leaderboard begins the week with 100 points, the difference is 50 points.  If our example player earns 100 points +&Delta;20%, they would be awarded 100 points plus twenty percent of the 50 point difference between them and the leader.  In other words, in this example, that player would earn 110 points (100+50&times;0.20).
+							<h5 class="about-title mb-30"><b>Baseline</b> Model</h5>
+							This model is intended to establish a points base for the first two weeks.<br /><br />
+							<ul class="unordered-list">		
+								<li><b>First Place:</b> 100 points</li>
+								<li><b>Second Place:</b> 90 points</li>
+								<li><b>Third Place:</b> 80 points</li>
+								<li><b>Fourth Place:</b> 70 points</li>
+								<li><b>Fifth Place:</b> 60 points</li>
+								<li><b>Sixth Place:</b> 50 points</li>
+								<li><b>Seventh Place:</b> 40 points</li>
+								<li><b>Eighth Place:</b> 30 points</li>
+								<li><b>Ninth Place:</b> 20 points</li>
+								<li><b>Tenth Place:</b> 10 points</li>
+							</ul><br /><br />
+							<h5 class="about-title mb-30"><b>Standard</b> Model</h5>
+							This model is intended to be used for the majority of the tournament; it includes a modest catch-up mechanic.<br /><br />
+							<ul class="unordered-list">		
+								<li><b>First Place:</b> 100 points +&Delta;25%</li>
+								<li><b>Second Place:</b> 90 points +&Delta;22½%</li>
+								<li><b>Third Place:</b> 80 points +&Delta;20%</li>
+								<li><b>Fourth Place:</b> 70 points +&Delta;17½%</li>
+								<li><b>Fifth Place:</b> 60 points +&Delta;15%</li>
+								<li><b>Sixth Place:</b> 50 points +&Delta;12½%</li>
+								<li><b>Seventh Place:</b> 40 points +&Delta;10%</li>
+								<li><b>Eighth Place:</b> 30 points +&Delta;7½%</li>
+								<li><b>Ninth Place:</b> 20 points +&Delta;5%</li>
+								<li><b>Tenth Place:</b> 10 points +&Delta;2½%</li>								
+							</ul><br /><br />
+							<h5 class="about-title mb-30"><b>Catch-Up</b> Model</h5>
+							This model is intended to be used for the final weeks of the tournament; it includes a significant catch-up mechanic.<br /><br />
+							<ul class="unordered-list">		
+								<li><b>First Place:</b> 150 points +&Delta;35%</li>
+								<li><b>Second Place:</b>135 points +&Delta;30%</li>
+								<li><b>Third Place:</b> 120 points +&Delta;25%</li>
+								<li><b>Fourth Place:</b>105 points +&Delta;22%</li>
+								<li><b>Fifth Place:</b>  90 points +&Delta;20%</li>
+								<li><b>Sixth Place:</b>  75 points +&Delta;17%</li>
+								<li><b>Seventh Place:</b> 60 points +&Delta;15%</li>
+								<li><b>Eighth Place:</b>  45 points +&Delta;12%</li>
+								<li><b>Ninth Place:</b> 30 points +&Delta;10%</li>
+								<li><b>Tenth Place:</b> 15 points +&Delta;5%</li>
+							</ul><br /><br />
+							<h5 class="about-title mb-30"><b>Foothold</b> Model</h5>
+							This model is only intended for use during extra bonus matches; it only includes a small point boost for competitors.<br /><br />
+							<ul class="unordered-list">		
+								<li><b>First Place:</b> 50 points</li>
+								<li><b>Second Place:</b>45 points</li>
+								<li><b>Third Place:</b> 40 points</li>
+								<li><b>Fourth Place:</b> 35 points</li>
+								<li><b>Fifth Place:</b>  30 points</li>
+								<li><b>Sixth Place:</b>  25 points</li>
+								<li><b>Seventh Place:</b> 20 points</li>
+								<li><b>Eighth Place:</b>  15 points</li>
+								<li><b>Ninth Place:</b> 10 points</li>
+								<li><b>Tenth Place:</b> 5 points</li>
+							</ul>
+							</p>						
+						</div>
+						<h3 class="about-title mb-30">Loadouts</h3>
+						<div class="col-lg-12">
+							<p align="justify">Just as with scoring models, there are multiple loadout rules that change from week to week.  Players may use any class or subclass each week, but may not change class, subclass, weapons, armour, gear, subclass perks, weapon perks, armour perks, gear perks, mods, game options, or any other loadout configuration once the match has begun.  Any player caught changing any of these options will be disqualified from the tournament and forfeit all points.</p> 
+								
+							<p align="justify">All players must issue the "?loadout" command in the #champions-rose-loadouts channel prior to match start. Here are <i>some</i> of the loadout rules that will be employed in the tournament:<br />
+							<ul class="unordered-list">		
+								<li><b>Anything Goes</b><br />
+									Players may use any weapons, armour, abilities, and exotics they possess.  Power and exotic weapons are permitted.</li>
+								<li><b>Heavy Prohibited</b><br />
+									Players may use any weapons, armour, abilities, and exotics they possess, except power weapons.  Collection of heavy ammo and/or dealing damage with power weapons is prohibited.  Exotic weapons and armour are permitted.</li>
+								<li><b>Curated Loadouts</b><br />
+									During curated weeks, players will restricted to certain weapon types in the permitted slots.  Additionally, certain curated loadouts will restrict the rarity of weapons permitted.  If a player does not have a valid weapon in a given slot to meet the curated loadout, they may not use <i>any</i> weapon in that slot.</li>
+							</ul>
+							</p>
+						</div>
+						<h3 class="about-title mb-30">Terms and Conditions</h3>
+						<div class="col-lg-12">
+							<p align="justify">No microphone or headset is required, as the participants are not required to speak to each other during the course of the matches.</p>
+							
+							<p align="justify">Any disagreement or conflict will be decided solely by the community admin team.  Additionally, we reserve the right to modify the terms or format of the contest or the standing of any player at any time to ensure the smooth operation of the tournament.</p>
+
+							<p align="justify">The first place winner will receive a code for US$10 in Silver on the platform of their choice and the title of "Grand Champion" in the clan Discord.  The second and third place finishers will receive the title of "Crucible Champion" in the clan Discord.  The community admin team reserves the right to substitute a prize of equal or greater value at any time.</p>
+						</div>
+					</div>
+				</div>
+			</section>	
+			<!-- End Generic Start -->		
+
+			<?php pageFooter(); ?>	
+			<?php javaScripts(); ?>
+		</body>
+	</html>
